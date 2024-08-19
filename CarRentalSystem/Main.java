4df04bded6729bbb186cdf4a78b3ccf84af77a20
ZeroFPSx <zohebrehman777@gmail.com>
2024-08-19 20:12:45 +0530

    Update Main.java

--- CarRentalSystem/Main.java
+++ CarRentalSystem/Main.java
@@ -3,8 +3,14 @@ package CarRentalSystem;
 public class Main {
   public static void main(String[] args) {
     CarRentalSystem rentalSystem = new CarRentalSystem();
-    Car car1 = new Car("C001", "Toyota", "Camry", 60);
+    Car car1 = new Car("C001", "Toyota", "Fortuner", 60);
+    Car car2 = new Car("C002", "Mahindra", "Scorpio", 40);
+    Car car3 = new Car("C003", "Tata", "Harrier", 60);
+    Car car4 = new Car("C004", "Suzuki", "Brezza", 30);
     rentalSystem.addCar(car1);
+    rentalSystem.addCar(car2);
+    rentalSystem.addCar(car3);
+    rentalSystem.addCar(car4);
     rentalSystem.menu();
   }
 }
